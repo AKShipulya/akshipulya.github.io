71d9d93774d1fbd30d76bdf6928e5c6695c8224f
Andrey <akshipulya@gmail.com>
2021-09-29 16:11:36 +0300

    initial commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(window).on('load', function(){
+
+    // vide.js - video background
+    $('#header').vide('./video/cover', {
+        bgColor: '#a18158'
+    })
+
+    $('#portfolio').vide('./video/cover-portfolio', {
+        bgColor: 'black'
+    })
+
+});
